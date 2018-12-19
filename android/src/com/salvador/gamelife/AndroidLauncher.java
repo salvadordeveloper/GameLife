be95739c47bdf15ae9f327f6a09aa0ad84ecc31a
salvador <salvadorvalverde0@gmail.com>
2018-12-19 10:47:59 -0700

    Initial commit

---
+++ android/src/com/salvador/gamelife/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.salvador.gamelife;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.salvador.gamelife.GameLife;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameLife(), config);
+	}
+}
---
+++ desktop/src/com/salvador/gamelife/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.salvador.gamelife.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.salvador.gamelife.GameLife;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameLife(), config);
+	}
+}
